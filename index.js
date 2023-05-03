78745a06af0e06676cdfaf6063c70b2cae06467e
thetamimahmed <wwwtamim000@gmail.com>
2023-05-03 16:46:39 +0600

    added chef json

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chef.json')
 
 app.get('/', (req, res)=>{
     res.send('Italian Chef Hunt Server Running')
+})
+
+app.get('/chefs', (req, res)=>{
+    res.send(chefs)
 })
 
 app.listen(port, () => {
